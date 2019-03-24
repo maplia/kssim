be8c581ffc5d800b041082f0e16eedf2106e0117
maplia <sie@maplia.jp>
2019-03-24 23:09:14 +0900

    Spring Initializr

---
+++ src/main/kotlin/jp/maplia/kssim/KssimApplication.kt
@@ -0,0 +1,11 @@
+package jp.maplia.kssim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KssimApplication
+
+fun main(args: Array<String>) {
+	runApplication<KssimApplication>(*args)
+}
